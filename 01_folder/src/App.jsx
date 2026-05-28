1414f31077d61333ac0f28086e9e322f9da5e336
piyushyadav69592 <piyushyadav969549@gmail.com>
2026-05-28 15:02:35 +0530

    new folder

---
+++ 01_folder/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+ return(<>
+ 
+  <h1>hello developer kaise ho i hope app achhhe hoge </h1>
+  <h2>hello developer kaise ho i hope app achhhe hoge </h2>
+  </>
+ )
+}
+
+export default App
